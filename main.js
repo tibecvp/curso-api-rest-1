4ef3d2706707508d2e2a24d665794a58830f914a
Tibe Calderon <tibecvp@gmail.com>
2022-06-12 21:49:39 -0500

    Refactor, andmaking changes to show 3 dogs instead of 1

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 console.log('Hello, world!');
 
-const URL = "https://api.thedogapi.com/v1/images/search";
+const API_URL = "https://api.thedogapi.com/v1/images/search?limit=3";
 
-callApi();
+async function reload() {
+    const res = await fetch(API_URL);
+    const data = await res.json();
 
-function callApi() {
-    fetch(URL)
-    .then(res => res.json())
-    .then(data => {
-        const img = document.querySelector('img');
-        img.src = data[0].url;
-    });
+    console.log(data);
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
 }
 
-function changeDog () {
-    callApi();
-}
+reload();
